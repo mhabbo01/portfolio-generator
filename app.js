51aa0ccd39fea09a37cb2d64c534f2c70e43ab72
Matt Habbo <matthabbo45@gmail.com>
2022-04-02 11:15:44 -0400

    added questions and ability to add new project

--- app.js
+++ app.js
@@ -1,24 +1,97 @@
 // the require statement is a built in function thats globally available in node.js
 // it allows the app.js file to access the fs modules functions through the fs assignment
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// or
-const [name, github] = profileDataArgs;
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
 
 
-// console.log(name, github);
-// console.log(generatePage(name, github));
-// fs.writefile takes three arguments
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+const inquirer = require('inquirer');
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name', // name of the answer
+            message: 'What is your name?' // question to the user
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+
+const promptProject = portfolioData => {
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    // if there's no projects array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData); 
+        if (projectData.confirmAddProject) { // evaluating the user response to whether they wish to add more projects
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });
