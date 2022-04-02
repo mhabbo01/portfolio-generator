ccba77a36c0f5f0226c8c94818037fb683fa8774
Matt Habbo <matthabbo45@gmail.com>
2022-04-02 11:37:17 -0400

    added in validate and when methods

--- app.js
+++ app.js
@@ -19,17 +19,46 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name', // name of the answer
-            message: 'What is your name?' // question to the user
+            message: 'What is your name?', // question to the user
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("please enter your name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: gitHubUser => {
+                if (gitHubUser) {
+                    return true;
+                } else {
+                    console.log("please enter your github username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({confirmAbout}) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -49,12 +78,28 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: projectName => {
+                if (projectName) {
+                    return true;
+                } else {
+                    console.log("please enter your project name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDesc => {
+                if (projectDesc) {
+                    return true;
+                } else {
+                    console.log("please enter a description!");
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -65,7 +110,15 @@ Add a New Project
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            validate: gitLink => {
+                if (gitLink) {
+                    return true;
+                } else {
+                    console.log("please enter your GitHub link!");
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
